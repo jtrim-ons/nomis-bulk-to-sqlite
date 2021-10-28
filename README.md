91e48ca412214acd501e478ff616a56dc893b9c7
James Trimble <jtrim.ons@gmail.com>
2021-10-28 18:09:51 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# A script for getting lots of bulk data from Nomis
+
+Random notes:
+- This might not be reliable!
+- ./download-data.sh will get lots of data
+- It will probably take an hour to download and about 10GB of disk space
---
+++ download-data.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -eu -o pipefail
+
+mkdir -p data
+mkdir -p metadata
+
+curl 'https://www.nomisweb.co.uk/census/2011/bulk/r2_2' > metadata/index.html
+
+cat metadata/index.html | grep -o '/output/census/2011/[^/]*_oa.zip' > metadata/files.txt
+sed 's/^/https:\/\/www.nomisweb.co.uk/' metadata/files.txt > metadata/urls.txt
+
+cat metadata/urls.txt | while read f; do
+    b=$(basename $f)
+    curl "$f" > data/$b
+    (cd data && unzip $b)
+done
