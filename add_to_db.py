e321b2f102e87423cd14e7b4760614aed287d5b4
James Trimble <jtrim.ons@gmail.com>
2021-11-01 12:43:41 +0000

    Tidy code a bit and add counts

--- add_to_db.py
+++ add_to_db.py
@@ -2,94 +2,93 @@ import csv
 import glob
 import sqlite3
 
-print("This isn't working yet!")
-exit(0)
-
-con = sqlite3.connect('census.db')
-
-cur = con.cursor()
-
-def csv_iter(filename):
-    with open(filename, newline='') as f:
-        csvreader = csv.reader(f)
-        for i, row in enumerate(csvreader):
-            if i == 0:
-                headings = row
-            else:
-                yield dict(zip(headings, row))
-
-tables = [
-    "COUNTS(place_id, year, val_id, count)",
+TABLES = [
+    "COUNTS(place_id, year, category_id, count)",
     "VARIABLES(var_name, population, nomis_table_code_2011)",
     "CATEGORIES(var_id, val_name, measurement_unit, stat_unit, nomis_code_2011)"
 ]
 
-for table in tables:
-    cur.execute('DROP TABLE IF EXISTS {}'.format(table[:table.find("(")]))
-for table in tables:
-    cur.execute('CREATE TABLE IF NOT EXISTS {}'.format(table))
+def csv_iter(filename):
+    with open(filename, newline='') as f:
+        yield from csv.DictReader(f)
 
-nomis_table_id_to_var_id = {}
+def create_tables(cur):
+    for table in TABLES:
+        cur.execute('DROP TABLE IF EXISTS {}'.format(table[:table.find("(")]))
+    for table in TABLES:
+        cur.execute('CREATE TABLE IF NOT EXISTS {}'.format(table))
 
-meta_files = glob.glob("data/**/*META*.CSV", recursive=True)
-for filename in meta_files:
-    for d in csv_iter(filename):
-        print(d)
-        cur.execute('insert into VARIABLES values (?,?,?)',
-            [d["DatasetTitle"], d["StatisticalPopulations"], d["DatasetId"]])
-        nomis_table_id_to_var_id[d["DatasetId"]] = cur.lastrowid
-    print()
+def add_meta_tables(cur):
+    nomis_table_id_to_var_id = {}
+    meta_files = glob.glob("data/**/*META*.CSV", recursive=True)
+    for filename in meta_files:
+        for d in csv_iter(filename):
+            print(d)
+            cur.execute('insert into VARIABLES values (?,?,?)',
+                [d["DatasetTitle"], d["StatisticalPopulations"], d["DatasetId"]])
+            nomis_table_id_to_var_id[d["DatasetId"]] = cur.lastrowid
+        print()
+    return nomis_table_id_to_var_id
 
-nomis_col_id_to_category_id = {}
-desc_files = glob.glob("data/**/*DESC*.CSV", recursive=True)
-print(desc_files)
-for filename in desc_files:
-    for d in csv_iter(filename):
-        print(d)
-        col_code = d["ColumnVariableCode"]
-        pos = col_code.rfind("EW")
-        if pos == -1:
-            continue
-        table_code = col_code[:pos+2]
-        print("TABLECODE", table_code)
-        var_id = nomis_table_id_to_var_id[table_code]
-        cur.execute('insert into CATEGORIES values (?,?,?,?,?)', [
-            var_id,
-            d["ColumnVariableDescription"],
-            d["ColumnVariableMeasurementUnit"],
-            d["ColumnVariableStatisticalUnit"],
-            d["ColumnVariableCode"]
-        ])
-        nomis_col_id_to_category_id[d["ColumnVariableCode"]] = cur.lastrowid
-    print()
+def add_desc_tables(cur, nomis_table_id_to_var_id):
+    nomis_col_id_to_category_id = {}
+    desc_files = glob.glob("data/**/*DESC*.CSV", recursive=True)
+    print(desc_files)
+    for filename in desc_files:
+        for d in csv_iter(filename):
+            print(d)
+            col_code = d["ColumnVariableCode"]
+            pos = col_code.rfind("EW")
+            if pos == -1:
+                continue
+            table_code = col_code[:pos+2]
+            print("TABLECODE", table_code)
+            var_id = nomis_table_id_to_var_id[table_code]
+            cur.execute('insert into CATEGORIES values (?,?,?,?,?)', [
+                var_id,
+                d["ColumnVariableDescription"],
+                d["ColumnVariableMeasurementUnit"],
+                d["ColumnVariableStatisticalUnit"],
+                d["ColumnVariableCode"]
+            ])
+            nomis_col_id_to_category_id[d["ColumnVariableCode"]] = cur.lastrowid
+        print()
+    return nomis_col_id_to_category_id
 
-print(nomis_table_id_to_var_id)
+def add_data_tables(cur, nomis_col_id_to_category_id):
+    for data_file_num in ["01", "02", "03", "04", "05", "06"]:
+        data_files = glob.glob("data/**/*DATA{}.CSV".format(data_file_num), recursive=True)
+        print(data_files)
+        for filename in data_files:
+            print(filename)
+            rows = []
+            for d in csv_iter(filename):
+                geog_code = d["GeographyCode"]
+                for column_code in d:
+                    if column_code == "GeographyCode":
+                        continue
+                    if column_code not in nomis_col_id_to_category_id:
+                        continue
+                    rows.append((
+                        geog_code,
+                        2011,
+                        nomis_col_id_to_category_id[column_code],
+                        d[column_code]
+                    ))
+            cur.executemany('insert into COUNTS values (?,?,?,?)', rows)
 
-data01_files = glob.glob("data/**/*DATA01.CSV", recursive=True)
-print(desc_files)
-for filename in desc_files:
-    for d in csv_iter(filename):
-        print(d)
-        col_code = d["ColumnVariableCode"]
-        pos = col_code.rfind("EW")
-        if pos == -1:
-            continue
-        table_code = col_code[:pos+2]
-        print("TABLECODE", table_code)
-        var_id = nomis_table_id_to_var_id[table_code]
-        cur.execute('insert into CATEGORIES values (?,?,?,?,?)', [
-            var_id,
-            d["ColumnVariableDescription"],
-            d["ColumnVariableMeasurementUnit"],
-            d["ColumnVariableStatisticalUnit"],
-            d["ColumnVariableCode"]
-        ])
-        nomis_col_id_to_category_id[d["ColumnVariableCode"]] = cur.lastrowid
-    print()
-#"COUNTS(place_id, year, val_id, count)"
-#"CATEGORIES(var_id, val_name, measurement_unit, stat_unit, nomis_code_2011)"
+def main():
+    con = sqlite3.connect('census.db')
+    cur = con.cursor()
 
-con.commit()
+    create_tables(cur)
 
-con.close()
+    nomis_table_id_to_var_id = add_meta_tables(cur)
+    nomis_col_id_to_category_id = add_desc_tables(cur, nomis_table_id_to_var_id)
+    add_data_tables(cur, nomis_col_id_to_category_id)
 
+    con.commit()
+    con.close()
+
+if __name__ == "__main__":
+    main()
