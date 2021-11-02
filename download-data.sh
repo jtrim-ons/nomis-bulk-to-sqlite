5311207142d4289d10ffe16aa41290b91b103d18
James Trimble <jtrim.ons@gmail.com>
2021-11-02 09:44:59 +0000

    Download even less data

--- download-data.sh
+++ download-data.sh
@@ -10,7 +10,7 @@ curl 'https://www.nomisweb.co.uk/census/2011/bulk/r2_2' > metadata/index.html
 cat metadata/index.html | grep -o '/output/census/2011/[^/]*_oa.zip' > metadata/files.txt
 sed 's/^/https:\/\/www.nomisweb.co.uk/' metadata/files.txt > metadata/urls.txt
 
-cat metadata/urls.txt | grep ks10 | while read f; do
+cat metadata/urls.txt | grep 'ks10[17]' | while read f; do
     b=$(basename $f)
     curl "$f" > data/$b
     (cd data && unzip $b)
